ed8052f0c9776353fdd6ad86afffdcf428052594
Beto <zonanabeto@gmail.com>
2018-05-07 16:23:14 -0500

    ya tiene paleta

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+hbs.registerPartials(__dirname + '/views/partials');
 
 
 
@@ -54,5 +55,6 @@ app.locals.title = 'Express - Generated with IronGenerator';
 const index = require('./routes/index');
 app.use('/', index);
 
+app.listen(3000,console.log("aqui andamos corriendo"))
 
 module.exports = app;
